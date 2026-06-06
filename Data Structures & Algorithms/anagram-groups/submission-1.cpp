97db4ed10c37a8461e74b39d9897b60f74f15e6f
Tarun Siva <thetarunsiva@gmail.com>
2026-06-06 16:19:23 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<int> pos(strs.size(), 0);
+        unordered_map<string, int> mpp;
+        int cc = 0;
+        for (int i = 0; i < strs.size(); i++) {
+            string curr = strs[i];
+            vector<int> a(26, 0);
+            for (char j : curr) {
+                int pp = int(j) - 97;
+                a[pp]++;
+            }
+            string s = "";
+            for (int i = 0; i < 26; i++) {
+                s += char(a[i]);
+            }
+            if (mpp.contains(s)) {
+                pos[i] = mpp[s];
+            }
+            else {
+                pos[i] = cc;
+                mpp[s] = pos[i];
+                cc++;
+            }
+        }
+        vector<vector<string>> ans(cc);
+        for (int i = 0; i < pos.size(); i++) { 
+            ans[pos[i]].push_back(strs[i]);
+        }
+        return ans;
+    }
+};
