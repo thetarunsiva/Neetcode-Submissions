33237cde7f21354105cfa76bd90c025df9c925c4
Tarun Siva <thetarunsiva@gmail.com>
2026-07-17 11:26:47 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> courses;
+    vector<int> process;
+    vector<int> a;
+    bool dfs(int c) {
+        if (process[c] == 2) {
+            return true;
+        }
+        if (courses[c].empty()) {
+            process[c] = 2;
+            a.push_back(c);
+            return true;
+        }
+        if (process[c] == 1) {
+            return false;
+        }
+        process[c] = 1;
+        for (int i = 0; i < courses[c].size(); i++) {
+            int prereq = courses[c][i];
+            if (dfs(prereq) == false) {
+                return false;
+            }
+        }
+        process[c] = 2;
+        a.push_back(c);
+        return true;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        courses = vector<vector<int>>(numCourses);
+        process = vector<int>(numCourses, 0);
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int course = prerequisites[i][0];
+            int prereq = prerequisites[i][1];
+            courses[course].push_back(prereq);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            bool completed = dfs(i);
+            if (completed == false) {
+                return {};
+            }
+        }
+        return a;
+    }
+};
+
